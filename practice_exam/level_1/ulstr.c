0ad2a20815f33b27e0553fad35d34e98c2192de4
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2024-08-01 08:06:06 -0300

    update ulstr.c

--- practice_exam/level_1/ulstr.c
+++ practice_exam/level_1/ulstr.c
@@ -10,7 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 int	main(int argc, char **argv)
 {
+	int i;
+
+	i = 0;
+	if (argc != 1)
+		write(1, "\n", 1);
+	else
+	{
+		while (*argv[1] != '\0')
+		{
+			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+			{
+				argv[1][i] -= 32;
+				i++;
+			}
+			else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+			{
+				argv[1][i] += 32;
+				i++;
+			}
+			else
+				i++;
+		}
+		write(1, "\n", 1);
+	}
 	return (0);
 }
